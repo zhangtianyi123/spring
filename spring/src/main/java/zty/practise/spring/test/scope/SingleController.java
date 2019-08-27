e84c1cc9a827bf35885b0b7a4cd74c09719e7b46
zhangty <465355874@qq.com>
2019-08-27 09:45:05 +0800

    增加基础的单例测试API

--- spring/src/main/java/zty/practise/spring/test/scope/SingleController.java
+++ spring/src/main/java/zty/practise/spring/test/scope/SingleController.java
@@ -1,8 +1,17 @@
 package zty.practise.spring.test.scope;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SingleController {
 
+	private int count = 0;
+	
+	@GetMapping("/test/single")
+	public Integer send() throws InterruptedException{
+		Thread.sleep(5000);
+		count++;
+		return count;
+	}
 }
