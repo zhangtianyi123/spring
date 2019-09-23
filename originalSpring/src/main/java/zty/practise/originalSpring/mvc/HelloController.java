7962489358498bb183ecc28f597b98aba9471b89
zhangty <465355874@qq.com>
2019-09-23 11:25:47 +0800

    HelloController

--- originalSpring/src/main/java/zty/practise/originalSpring/mvc/HelloController.java
+++ originalSpring/src/main/java/zty/practise/originalSpring/mvc/HelloController.java
@@ -7,12 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 	
-	/**
-	 * @ResponseBody 决定是否被解析为view
-	 * @return
-	 */
     @RequestMapping("/hello")
-//    @ResponseBody
     public String hello(){
         return "hello";
     }
